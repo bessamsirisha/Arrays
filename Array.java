11aeaa0a0c80ffce6f69e374656ba351d84670e8
bessamsirisha <bessamsirisha251@gmail.com>
2025-01-30 13:34:17 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,9 @@
+public class Array {
+    public static void main(String[] args) {
+        int array []={1,2,3,4,5};
+        System.out.println(array.length);
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+    }
+}
